7af7a80606a66aba7e0cad28d7ad5fea82da1561
jose-rgb <josericardosaraiva7@gmail.com>
2022-05-15 16:09:29 -0300

    adicionado Divider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,22 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Heading } from "@chakra-ui/react";
 import Banner from "../components/Banner";
+import Divider from "../components/Divider";
 import Header from "../components/Header";
 import TravelMode from "../components/TravelMode";
 
 export default function Home() {
   return (
     <Flex direction="column">
+
         <Header />
         <Banner />
         <TravelMode />
+        <Divider />
+
+        <Heading textAlign="center" fontWeight="500" mb={["5", "14"]} fontSize={["lg", "3xl", "4xl"]} >
+          Vamos nessa? <br /> Então escolha seu continente
+        </Heading>
+
     </Flex>
   )
 }
